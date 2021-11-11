d2c8331a1ea64d663479b32f5f8364fa276b5f6d
Marguerite Brugger <margueritebrugger@Marguerites-MacBook-Air.local>
2021-11-10 19:24:11 -0600

    reformatted calendar page

--- src/modules/pages/MyCalendarPage/Component.tsx
+++ src/modules/pages/MyCalendarPage/Component.tsx
@@ -1,15 +1,22 @@
 import React, { useCallback, useContext } from 'react';
-import { Calendar, Layout, Badge, Row, Col, Typography } from 'antd';
+import { Calendar, Layout, Badge, Row, Col, Typography, List } from 'antd';
 import { PresetStatusColorType } from 'antd/lib/_util/colors.d';
+import Item from 'antd/lib/list/Item';
 import { GlobalContext } from '../../root/GlobalStore';
 import { User } from '../../general/types';
 import './mycalendarpage.css';
 
 const { Content } = Layout;
-const { Title } = Typography;
+const { Title, Text } = Typography;
 function MyCalendarPage(): React.ReactElement {
   const { globalState } = useContext(GlobalContext);
   const user = globalState.loggedInUser as User;
+
+  const courseData = [
+    'Adventure Name 1',
+    'Adventure Name 2',
+    'Adventure Name 3',
+  ];
 
   const getListData = useCallback((value) => {
     let listData;
@@ -39,25 +46,6 @@ function MyCalendarPage(): React.ReactElement {
         break;
       default:
     }
-
-    return listData || [];
-  }, []);
-
-  const dateCellRender = useCallback((value) => {
-    const listData = getListData(value);
-
-    return (
-      <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
-            <Badge
-              status={item.type as PresetStatusColorType}
-              text={item.content}
-            />
-          </li>
-        ))}
-      </ul>
-    );
   }, []);
 
   return (
@@ -70,9 +58,56 @@ function MyCalendarPage(): React.ReactElement {
         </Col>
       </Row>
       <Row className="calendar-row">
-        <Col span={24}>
-          <Calendar dateCellRender={dateCellRender} className="calendar" />
+        <Col span={2} />
+        <Col span={9}>
+          <Calendar
+            fullscreen={false}
+            className="calendar"
+            dateFullCellRender={(current) => {
+              const style = {
+                border: '',
+                height: '8vh',
+                width: '8vh',
+                marginLeft: '2vh',
+                paddingTop: '2vh',
+                fontSize: '1.3em',
+              };
+              if (current.date() === 10 || current.date() === 15) {
+                style.border = '1px solid #000000';
+              }
+              return (
+                <div className="ant-picker-cell-inner" style={style}>
+                  {current.date()}
+                </div>
+              );
+            }}
+          />
         </Col>
+        <Col span={2} />
+        <Col span={9}>
+          <Title level={3}>November 1st, 2021</Title>
+          <List
+            dataSource={courseData}
+            renderItem={(item) => (
+              <List.Item>
+                <Typography.Text style={{ fontWeight: 'bold' }}>
+                  {' '}
+                  {item}{' '}
+                </Typography.Text>
+                .
+                <List size="small">
+                  <List.Item style={{ padding: 0 }}>
+                    Assignment 1 Name
+                  </List.Item>
+                  <List.Item style={{ padding: 0 }}>
+                    Assignment 2 Name
+                  </List.Item>
+                </List>
+              </List.Item>
+            )}
+          />
+        </Col>
+        <Col span={2} />
       </Row>
     </Content>
   );
